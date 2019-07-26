78c961c2ffb1c511e1e1191314f0ad168c491a90
0xf0f <0x0meta@gmail.com>
2019-07-27 05:01:27 +1200

    added FileIndexNode to package namespace

--- sqlite_file_index/__init__.py
+++ sqlite_file_index/__init__.py
@@ -1,7 +1,9 @@
 from .file_index import FileIndex
+from .file_index_node import FileIndexNode
 from .file_index_task import FileIndexTask
 
 __all__ = [
     'FileIndex',
+    'FileIndexNode',
     'FileIndexTask',
 ]
