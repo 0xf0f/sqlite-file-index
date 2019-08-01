330be90144ba8f53c6c1437208e949ed3452987c
0xf0f <0x0meta@gmail.com>
2019-08-02 04:17:59 +1200

    preparing package for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name='sqlite-file-index',
     version='1.4',
@@ -8,5 +11,12 @@ setup(
     license='MIT',
     author='0xf0f',
     author_email='',
-    description='Hierarchical file index in an sqlite database.'
+    description='Hierarchical file index in an sqlite database.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
--- sqlite_file_index/__init__.py
+++ sqlite_file_index/__init__.py
@@ -2,6 +2,8 @@ from .file_index import FileIndex
 from .file_index_node import FileIndexNode
 from .file_index_task import FileIndexTask
 
+name = 'sqlite_file_index'
+
 __all__ = [
     'FileIndex',
     'FileIndexNode',
