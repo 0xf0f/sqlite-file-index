1a9fd57358efcf8bb5ebae9bcbfb28861e6afe60
0xf0f <0x0meta@gmail.com>
2019-08-05 04:43:16 +1200

    added index attribute to FileIndexTag

--- sqlite_file_index/file_index_tag.py
+++ sqlite_file_index/file_index_tag.py
@@ -1,7 +1,12 @@
 import sqlite3
+from typing import TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from .file_index import FileIndex
 
 
 class FileIndexTag:
-    def __init__(self, row: sqlite3.Row):
+    def __init__(self, index: 'FileIndex', row: Union[dict, sqlite3.Row]):
+        self.index: 'FileIndex' = index
         self.id = row['id']
         self.name = row['name']
