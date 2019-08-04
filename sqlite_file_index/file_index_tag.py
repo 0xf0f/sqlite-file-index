ff5cb5fa780085fe7ae13a9cb8d7fab74ff420e2
0xf0f <0x0meta@gmail.com>
2019-08-05 04:46:32 +1200

    implemented get_files_with_tag method in FileIndexTag

--- sqlite_file_index/file_index_tag.py
+++ sqlite_file_index/file_index_tag.py
@@ -10,3 +10,20 @@ class FileIndexTag:
         self.index: 'FileIndex' = index
         self.id = row['id']
         self.name = row['name']
+
+    def get_files_with_tag(self):
+        rows = self.index.db.execute(
+            '''
+            select * from files where id in (
+               select file_id from file_tags where tag_id=?
+            )
+            
+            order by path collate nocase asc
+            ''',
+            (self.id,)
+        )
+
+        yield from map(
+            self.index.new_node, rows
+        )
+
