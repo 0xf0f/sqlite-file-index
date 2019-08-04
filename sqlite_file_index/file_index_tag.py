90b2b580c8de5ea6ad974669c6ae47975987115e
0xf0f <0x0meta@gmail.com>
2019-08-05 04:58:34 +1200

    moved sqlite3 import in file_index_tag to TYPE_CHECKING

--- sqlite_file_index/file_index_tag.py
+++ sqlite_file_index/file_index_tag.py
@@ -1,12 +1,12 @@
-import sqlite3
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
+    import sqlite3
     from .file_index import FileIndex
 
 
 class FileIndexTag:
-    def __init__(self, index: 'FileIndex', row: Union[dict, sqlite3.Row]):
+    def __init__(self, index: 'FileIndex', row: Union[dict, 'sqlite3.Row']):
         self.index: 'FileIndex' = index
         self.id = row['id']
         self.name = row['name']
