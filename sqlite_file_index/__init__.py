b2d041f2a3437c376dc4b903bbb9de7a338bacb6
0xf0f <0x0meta@gmail.com>
2019-08-07 04:54:31 +1200

    added version to __init__, moved name

--- sqlite_file_index/__init__.py
+++ sqlite_file_index/__init__.py
@@ -2,10 +2,15 @@ from .file_index import FileIndex
 from .file_index_node import FileIndexNode
 from .file_index_task import FileIndexTask
 
-name = 'sqlite_file_index'
-
 __all__ = [
     'FileIndex',
     'FileIndexNode',
     'FileIndexTask',
 ]
+
+name = 'sqlite_file_index'
+
+version_components = (1, 4, 1)
+version = '.'.join(
+    map(str, version_components)
+)
